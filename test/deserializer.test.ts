296056b8c0acc70454f7dc9edc1f040870ecea4a
Juan Aguilar Santillana <mhpoin@gmail.com>
2022-08-31 16:38:45 +0200

    fix: mimor issues

--- test/deserializer.test.ts
+++ test/deserializer.test.ts
@@ -2,18 +2,32 @@ import { readFileSync } from 'node:fs';
 import { test, expect } from 'vitest';
 
 import { fixPrismaFile } from '../src/fixer';
+import { join } from 'node:path';
 
+const configPath = join(__dirname, '../../schema-trans.mjs');
 test('deserialized simple', () =>
   expect(
-    fixPrismaFile(readFileSync('./fixtures/simple.prisma', 'utf-8')),
+    fixPrismaFile(
+      readFileSync('./fixtures/simple.prisma', 'utf-8'),
+      [],
+      configPath,
+    ),
   ).resolves.toMatchSnapshot());
 
 test('deserialized schema', () =>
   expect(
-    fixPrismaFile(readFileSync('./fixtures/schema.prisma', 'utf-8')),
+    fixPrismaFile(
+      readFileSync('./fixtures/schema.prisma', 'utf-8'),
+      [],
+      configPath,
+    ),
   ).resolves.toMatchSnapshot());
 
 test('deserialized blog', () =>
   expect(
-    fixPrismaFile(readFileSync('./fixtures/blog.prisma', 'utf-8')),
+    fixPrismaFile(
+      readFileSync('./fixtures/blog.prisma', 'utf-8'),
+      [],
+      configPath,
+    ),
   ).resolves.toMatchSnapshot());
